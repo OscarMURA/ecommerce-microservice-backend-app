9606cfa182d5f8682bb966730c8f6bf7d355e092
Oscar Stiven Muñoz Ramirez <1083870123@u.icesi.edu.co>
2025-10-30 12:57:27 -0500

    fix(ci): escapar  en jq y resolver IPs de VMs en favourite/shipping Ensure VM Available (igual a product/service-discovery)

--- favourite-service/jenkins/favourite-service-dev.groovy
+++ favourite-service/jenkins/favourite-service-dev.groovy
@@ -174,7 +174,7 @@ pipeline {
               sh(script: """
 set -e
 curl -sS -H "Authorization: Bearer ${DO_TOKEN}" "https://api.digitalocean.com/v2/droplets?per_page=200" \
-  | jq -r --arg NAME "${vmName}" '.droplets[] | select(.name==$NAME) | .networks.v4[] | select(.type=="public") | .ip_address' \
+  | jq -r --arg NAME \"${vmName}\" '.droplets[] | select(.name==\$NAME) | .networks.v4[] | select(.type==\"public\") | .ip_address' \
   | head -n1
 """, returnStdout: true).trim()
             }
