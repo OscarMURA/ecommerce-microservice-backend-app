2e6da364190478dd1151064d1d3e9491287c1ecf
Oscar Stiven Muñoz Ramirez <1083870123@u.icesi.edu.co>
2025-10-30 11:33:49 -0500

    fix(ci): mover stage 'Wait for Service Discovery' dentro de 'stages' en payment-service-dev

--- payment-service/jenkins/payment-service-dev.groovy
+++ payment-service/jenkins/payment-service-dev.groovy
@@ -917,7 +917,6 @@ jenkins/scripts/deploy-single-service-to-gke.sh
         }
       }
     }
-  }
 
     stage('Wait for Service Discovery') {
       when {
@@ -958,6 +957,8 @@ exit 1
         }
       }
     }
+
+  }
 
   post {
     success {
